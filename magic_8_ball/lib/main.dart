4c605f5dcc10fdebe213fc28276eac9f0c6d0cdc
chupichapa <pasionharold01@gmail.com>
2024-11-18 19:50:48 +0800

    finalize
    dsds

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,20 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+void main() => runApp(
+      const MaterialApp(
+        home: BallPage(),
+      ),
+    );
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+// Creates a Scaffold with
+// appbar using Stateless widget
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[100],
+      appBar: AppBar(
+        backgroundColor: Colors.green[600],
+        title: const Text('Ball'),
+      ),
+      body: const Ball(),
+    );
+  }
+}
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            // Random.nextInt(n) returns a random integer from 0 to n-1
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        // Adding images
+        child: Image.asset('assets/images/ball$ballNumber.png'),
+      ),
+    );
+  }
 }
