6401a38425d272652bf842a6d61eb059e29c7e15
chupichapa <pasionharold01@gmail.com>
2024-11-19 12:16:49 +0800

    fas
    ddsad

---
+++ basic_quiz_app/lib/quiz.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import './answer.dart';
+import './question.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final Function answerQuestion;
+
+  Quiz({
+    super.key,
+    required this.questions,
+    required this.answerQuestion,
+    required this.questionIndex,
+  }) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(
+          questions[questionIndex]['questionText'].toString(),
+        ), // Question
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(
+            selectHandler: _answerQuestion, // Pass the callback function
+            answerText: answer['text'].toString(),
+          );
+        }).toList(),
+      ],
+    ); // Column
+  }
+
+  // Callback function to handle answer questions
+  void _answerQuestion(int score) {
+    answerQuestion(score);
+  }
+}
